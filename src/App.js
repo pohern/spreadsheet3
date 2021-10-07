3902b084b53b8b0fe8318998ab7a5648b615c96f
pohern <ohern_p@yahoo.com>
2021-10-07 13:22:04 -0500

    update spreadhseet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDataSheet from "react-datasheet";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [
+        [{ value: 1 }, { value: 3 }],
+        [{ value: 2 }, { value: 4 }],
+      ],
+    };
+  }
+  render() {
+    return (
+      <ReactDataSheet
+        data={this.state.grid}
+        valueRenderer={(cell) => cell.value}
+        onCellsChanged={(changes) => {
+          const grid = this.state.grid.map((row) => [...row]);
+          changes.forEach(({ cell, row, col, value }) => {
+            grid[row][col] = { ...grid[row][col], value };
+          });
+          this.setState({ grid });
+        }}
+      />
+    );
+  }
 }
 
-export default App;
+export default App
